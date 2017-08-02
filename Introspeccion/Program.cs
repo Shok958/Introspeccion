f4aa4418cdcf054fb60ee61dad964a78a2851488
Antonio <antonio958@hotmail.com>
2017-08-02 13:32:19 +0200

    Lo que me ha dado tiempo a copiar

--- Introspeccion/Program.cs
+++ Introspeccion/Program.cs
@@ -10,6 +10,56 @@ namespace Introspeccion
     {
         static void Main(string[] args)
         {
+            var persons = new List<Person>()
+            {
+                new Person(){ Id = 1, Name = "Pedro"},
+                new Person(){ Id = 2 }
+            };
+
+            //validate persons
+            foreach (var person in persons)
+            {
+                var type = person.GetType();
+                var properties = type.GetProperties();
+            }
+            //convierte a object y muestra su metainformacion
         }
+    }
+
+    public class Generic
+    {
+         
+    }
+
+    public class Person {
+
+        public int Id { get; set; }
+        [StringDatabase(Length = 50)]
+        public string Name { get; set; }
+
+        public void Save()
+        {
+
+        }
+    }
+    public interface IValid{
+        bool isValid(String value);
+     }
+
+    public class StringDatabaseAttribute : Attribute {
+
+        public int Length { get; set; }
+        public bool Required { get; set; }
+
+        public bool isValid(String value)
+        {
+            return true;
+        }
+
+        public static List<Generic> ToGeneric(IEnumerable<Object> list) {
+
+
+        }
+
     }
 }
